49e638e6e797d65bc3804adfc7917711bf636028
buckn <nathanieljbuck7@gmail.com>
2019-12-31 00:15:14 -0500

    fixed steam paths not existing

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,11 @@ mod tests {
         x.add_profile();
         x.remove_profile(1);
         x.set_platform();
-        x.add_steam_folder("/Users/nathanielbuck/Library/Application Support/Steam/".to_string());
-        x.add_steam_folder("/Users/nathanielbuck/Library/Application Support/Steam/".to_string());
+        x.add_steam_folder("/tmp".to_string());
+        x.add_steam_folder("/tmp".to_string());
         x.remove_steam_folder_at_index(0);
         println!("Steam Folder: {:?}", x.get_steam_folder_at_pointer(0));
         x.set_paths();
-        x.fs_read_all_game_sens_at_index(0);
-        x.fs_read_game_sens_at_index(SupportedGames::CSGO, 0);
         x.equalize_profile_at_index(SupportedGames::CSGO, 0);
         x.switch_profile(0);
         x.fs_save_profiles();
